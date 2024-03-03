16d3f8058e8d226eb0257fa5c35bb71783bb40ad
kimkiyong <fenderst0612@gmail.com>
2024-03-03 21:42:37 +0900

    feat: update prompt

--- prompt/const.go
+++ prompt/const.go
@@ -2,9 +2,8 @@ package prompt
 
 const (
 	GenQuote = `
-	## markdown形式で以下の内容を出力する
-	- 示唆を与えてくれる偉人の名言を1つ生成する
-	- その偉人の名前・詳細を表示する
-	- その名言が生まれた背景を説明する
+	## Please output the following in markdown format in Japanese.
+	- Generate one quote from a great person.
+	- Describe the name and details of the great man and the context in which the quote came about..
 	`
 )
