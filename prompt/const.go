6304a5d4255e3bbfa8110b61c4fb18fedbcff050
kimkiyong <fenderst0612@gmail.com>
2024-06-22 15:11:08 +0900

    chore: prompt

--- prompt/const.go
+++ prompt/const.go
@@ -7,5 +7,16 @@ const (
 	
 [restriction]
 - Generate one quote from a great person.
-- Describe the name and details of the great man and the context in which the quote came about.`
+- Describe the name and details of the great man and the context in which the quote came about.
+
+[format]
+- The output should be in markdown format.
+- The output should be in Japanese.
+- The output should be in the following format.
+	- Name of
+	- Details
+	- Context
+	- Quote
+	- Source
+`
 )
