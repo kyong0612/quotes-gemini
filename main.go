fdf88ca1d536ae211b9f5b8a9a66826c692bba14
kimkiyong <fenderst0612@gmail.com>
2025-05-14 08:45:43 +0900

    Feature/update prompt (#105)
    * feat(main): 日本時間を考慮したポジティブメッセージ生成機能を追加し、定数を更新
    
    * fix(prompt): 定数のマークダウン形式を修正し、出力内容を整理

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/kyong0612/quotes-gemini/internal/gemini"
 	"github.com/kyong0612/quotes-gemini/internal/githubgist"
@@ -13,15 +14,21 @@ import (
 func main() {
 	ctx := context.Background()
 
+	// Load Asia/Tokyo location
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(fmt.Errorf("failed to load location: %w", err))
+	}
+
 	// generate quote by gemini
-	gemini, err := gemini.NewClient(ctx)
+	geminiClient, err := gemini.NewClient(ctx)
 	if err != nil {
 		log.Fatal(fmt.Errorf("failed to create gemini client: %w", err))
 	}
 
-	defer gemini.Close()
+	defer geminiClient.Close()
 
-	quote, err := gemini.GenerateContent(ctx, prompt.GenQuote)
+	quote, err := geminiClient.GenerateContent(ctx, fmt.Sprintf(prompt.GenPositiveMessage, time.Now().In(jst).Format("2006年1月2日")))
 	if err != nil {
 		log.Fatal(fmt.Errorf("failed to generate content: %w", err))
 	}
@@ -29,12 +36,12 @@ func main() {
 	fmt.Println(quote)
 
 	// post to github gist
-	githubgist, err := githubgist.NewClient()
+	gistClient, err := githubgist.NewClient()
 	if err != nil {
 		log.Fatal(fmt.Errorf("failed to create githubgist client: %w", err))
 	}
 
-	if err := githubgist.EditGist(ctx, "6016b8dd4f5d61f9cfcbbc8443d55260", "quote.md", quote); err != nil {
+	if err := gistClient.EditGist(ctx, "6016b8dd4f5d61f9cfcbbc8443d55260", "quote.md", quote); err != nil {
 		log.Fatal(fmt.Errorf("failed to edit gist: %w", err))
 	}
 
